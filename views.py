6037d50d514801502305aca52b8187013633029b
Vlagimir Ivanov <volnyivolodya@gmail.com>
2024-09-19 12:34:10 +0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+@login_required
+def profile(request):
+    user = request.user
+    return render(request, 'profile.html', {'user': user})
+
