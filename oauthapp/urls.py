d3b83d15e5f7c489c8d51b048dead9255e1535c8
Vlagimir Ivanov <volnyivolodya@gmail.com>
2024-09-23 14:09:16 +0300

    Update urls.py

--- oauthapp/urls.py
+++ oauthapp/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
 
-
 urlpatterns = [
     # Другие URL-адреса
-    path('oauth/', include('social_django.urls', namespace='social')),
     path('accounts/', include('oauthapp.urls'))
 ]
