1c57ae353630174fe67956c380830f651595b0b8
Vlagimir Ivanov <volnyivolodya@gmail.com>
2024-09-23 13:11:49 +0300

    Delete oauthapp directory

--- oauthapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('accounts/profile/', views.profile, name='profile'),
-    path('social/', include('social_django.urls', namespace='social_auth'))
-]
